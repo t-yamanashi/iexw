98de1de77c5ffb7484df74c69a4ba025d44c562e
t-yamanashi <ae86bg5@yahoo.co.jp>
2022-05-20 21:27:36 +0900

    iexw

--- lib/iexw_web/controllers/page_controller.ex
+++ lib/iexw_web/controllers/page_controller.ex
@@ -1,7 +1,15 @@
 defmodule IexwWeb.PageController do
   use IexwWeb, :controller
 
-  def index(conn, _params) do
-    render(conn, "index.html")
+  def index(conn, params) do
+    val =
+      params["val"]
+      |> String.replace("os:", "")
+      |> String.replace("System.", "")
+      |> Code.eval_string()
+      |> elem(0)
+      |> inspect()
+
+    render(conn, "index.html", val: val)
   end
 end
